56a091e2e1fcbc00045d821d391651f34a5c897e
Micah R Ledbetter <me@micahrl.com>
2021-01-11 19:22:27 -0600

    Add Fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("TMDYZOTC", {
+      includedDomains: ["cistercian.micahrl.com"],
+    });
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
