005a6ff47acb3ab61235053aecf23bba035f2971
Micah R Ledbetter <me@micahrl.com>
2021-01-12 01:51:34 -0600

    Reject invalid input, normalize number
    * If a valid number is present and the user types an invalid character
      (e.g. 2 => 2f), reject the invalid character (the text box will just
      have a 2 in it)
    * Note that if the text box is empty, it will allow letters, but the
      prompt will still say "Enter a number"
    * Normalize the number to remove leading zeros, e.g. 01 => 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,57 @@
 import Link from "next/link";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import SiteHead from "../components/SiteHead";
 import CistercianNumeralDisplay from "../components/CistercianNumeralDisplay";
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>();
   const [num, setNum] = useState<number>(NaN);
+
+  /* Set the value in the input box
+   */
+  const setInputVal = (newVal: string) => {
+    if (!inputRef || !inputRef.current) {
+      console.log(
+        `setInputVal(${newVal}): no-op, inputRef.current is undefined`
+      );
+      return;
+    }
+    console.log(`setInputVal(${newVal}): updaing input box...`);
+    inputRef.current.value = newVal;
+  };
 
   /* Allow only integers from 0-9999
    */
   const processInputNumber = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(
-      `processInputNumber(): event.target.value: ${
-        event.target.value
-      } ${typeof event.target.value})`
-    );
+    // console.table(event.target);
     if (event.target.value === "") {
-      console.log("processInputNumber(): input field is empty");
-      setNum(NaN);
+      if (event.target.validity.badInput) {
+        console.log(
+          "processInputNumber(): invalid character added to input field"
+        );
+        // Set the text box contents to the old value -- ignore the new invalid character
+        setInputVal(String(num));
+        setNum(num);
+      } else {
+        console.log("processInputNumber(): input field is empty");
+        setInputVal("");
+        setNum(NaN);
+      }
       return;
     }
-    const num = Number(event.target.value);
-    console.log(`processInputNumber() => ${num} (${typeof num})`);
-    if (!Number.isInteger(num)) {
+    const newNum = Number(event.target.value);
+    console.log(`processInputNumber() => ${newNum} (${typeof newNum})`);
+    if (!Number.isInteger(newNum)) {
       console.log("Input error: non-integer number");
       return;
-    } else if (num < 0 || num > 9999) {
+    } else if (newNum < 0 || newNum > 9999) {
       console.log("Input error: number is outside of range 0-9999");
       return;
     }
-    setNum(num);
+    // Why setInputVal here with a string?
+    // This normalizes the number, so that e.g. 01 becomes just 1.
+    setInputVal(String(newNum));
+    setNum(newNum);
   };
 
   return (
@@ -64,10 +87,12 @@ export default function Home() {
         <div className="py-4 flex flex-1 flex-col justify-center items-center">
           <p className="py-2">Type an integer between 0-9999:</p>
           <input
+            ref={inputRef}
             className="border border-black p-2"
             type="number"
             value={isNaN(num) ? "" : num}
             inputMode="numeric"
+            pattern="\d*"
             onChange={processInputNumber}
           />
         </div>
