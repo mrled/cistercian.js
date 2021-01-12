db517c2ff2d75798ff8c1855c940dc8cd3aeaed5
Micah R Ledbetter <me@micahrl.com>
2021-01-12 01:17:13 -0600

    Add missing CistercianNumeralDisplay

---
+++ components/CistercianNumeralDisplay.tsx
@@ -0,0 +1,30 @@
+import { number2cistercian } from "../lib/cistercian";
+
+type CistercianNumeralDisplayProps = {
+  num: number;
+};
+export default function CistercianNumeralDisplay({
+  num,
+}: CistercianNumeralDisplayProps) {
+  if (isNaN(num)) {
+    return (
+      <div className="py-8 flex flex-1 flex-col justify-center items-center">
+        <p>Enter a number</p>
+      </div>
+    );
+  } else {
+    const cistercian = number2cistercian(num);
+    return (
+      <div className="py-8 flex flex-1 flex-col justify-center items-center">
+        <p>
+          The decimal number{" "}
+          <span className="text-lg text-gray-400">{num}</span> in Cistercian
+          notation:
+        </p>
+        <p className="py-8 text-9xl font-cistercian items-center">
+          {cistercian}
+        </p>
+      </div>
+    );
+  }
+}
